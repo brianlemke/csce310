8e8f063cea0558bd1186c6d13be639e6a09b218f
Thomas Macaulay <thomas.p.macaulay@gmail.com>
2013-04-16 20:16:14 -0500

    Update and rename update_large_employee to update_large_employee.sql

---
+++ part_4/queries/update_large_employee.sql
@@ -0,0 +1,4 @@
+select * from Employee
+update Employee set title='Professional Librarian', salary = 9700.25
+where lastName='Schiller' and firstName = 'Clark' and libraryName = 'Reynolds and Sons'
+select * from Employee
